ac70157a30ff1abeaa2dd97894cf0a7bbfebb53d
izkai <iirzynska@gmail.com>
2015-01-13 12:16:42 +0100

    change of user and of the gitignore settings

--- src/com/iza/HelloWorld.java
+++ src/com/iza/HelloWorld.java
@@ -2,6 +2,6 @@ package com.iza;
 
 public class HelloWorld {
 	public static void main( String[] args ){
-		System.out.println("Hello World!");
+		System.out.println("Hello World - change of user!");
 	}
 }
